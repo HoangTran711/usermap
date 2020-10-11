2f2f73c671043e645527187f1e167eb0eeb09b71
KhoaPham-dev <superstars1860@gmail.com>
2020-10-11 21:43:54 +0700

    search room

--- src/App.js
+++ src/App.js
@@ -1,173 +1,179 @@
-import React,{useState,useEffect,useContext} from 'react';
-import './css/style.css';
-import Sidebar1 from './components/Sidebar1/Sidebar1';
-import MyContext from './context/MyContext';
-import MapBox from './components/mapbox/Mapbox';
-import Menu from './components/Menu/Menu';
-
+import React, { useState, useEffect, useContext } from "react";
+import "./css/style.css";
+import Sidebar1 from "./components/Sidebar1/Sidebar1";
+import MyContext from "./context/MyContext";
+import MapBox from "./components/mapbox/Mapbox";
+import Menu from "./components/Menu/Menu";
 
 function App() {
-  const [mode,setMode] = useState(0);
+  const [mode, setMode] = useState(0);
   const [dataApi, setDataApi] = useState(null);
-  const [isLoading,setLoading] = useState(false);
+  const [isLoading, setLoading] = useState(false);
   const [prevIdEnd, setPrevIdEnd] = useState([]);
-  const [prevIdStart,setPrevIdStart] = useState([]);
-  const [eventLis,setEventLis] = useState(null);
+  const [prevIdStart, setPrevIdStart] = useState([]);
+  const [eventLis, setEventLis] = useState(null);
   const [geoLoc, setGeoLoc] = useState(null);
-const [userLocation,setUserLocation] = useState({
-  geometry:{
-      coordinates:[]
-  }
-});
-  const [start,setStart] = useState({
-    type:"Feature",
-    properties:{
-      id:null,
-      name:null,
-      description:null,
-      image:null
+  const [userLocation, setUserLocation] = useState({
+    geometry: {
+      coordinates: [],
+    },
+  });
+  const [start, setStart] = useState({
+    type: "Feature",
+    properties: {
+      id: null,
+      name: null,
+      description: null,
+      image: null,
     },
     geometry: {
       coordinates: [],
-    }
+    },
   });
-  const [end,setEnd] = useState({
-    type:"Feature",
-    properties:{
-      id:null,
-      name:null,
-      description:null,
-      image:null
+  const [end, setEnd] = useState({
+    type: "Feature",
+    properties: {
+      id: null,
+      name: null,
+      description: null,
+      image: null,
     },
     geometry: {
       coordinates: [],
-    }
+    },
   });
-  const [direction , setDirection] = useState(null);
-  const [test,setTest] = useState(null);
+  const [direction, setDirection] = useState(null);
+  const [test, setTest] = useState(null);
   useEffect(() => {
     var t;
-    if(geoLoc) {
+    if (geoLoc) {
       clearInterval(t);
     } else {
       t = setInterval(() => {
-        fetch('http://localhost:8080/admin')
-        .then(res => res.json())
-        .then (result => {
-          for(let i = 0; i < result.features.length; i++) {
-            for(let j = i+1; j < result.features.length; j++) {
-              if(result.features[i].properties.qty_search < result.features[j].properties.qty_search) {
-                let temp = result.features[i];
-                result.features[i] = result.features[j];
-                result.features[j] = temp;
+        fetch("https://utemap.com/admin")
+          .then((res) => res.json())
+          .then((result) => {
+            for (let i = 0; i < result.features.length; i++) {
+              for (let j = i + 1; j < result.features.length; j++) {
+                if (
+                  result.features[i].properties.qty_search <
+                  result.features[j].properties.qty_search
+                ) {
+                  let temp = result.features[i];
+                  result.features[i] = result.features[j];
+                  result.features[j] = temp;
+                }
               }
             }
-          }
-          console.log(result.features);
-          setGeoLoc({
-            type:"FeatureCollection",
-            features: result.features
-          })
-        })
-      },1000)
+            console.log(result.features);
+            setGeoLoc({
+              type: "FeatureCollection",
+              features: result.features,
+            });
+          });
+      }, 1000);
       setTest(t);
     }
     return () => {
       clearInterval(t);
-    }
-    
-
-  },[geoLoc])
+    };
+  }, [geoLoc]);
   useEffect(() => {
-    fetch('http://localhost:8080/admin')
-    .then(res => res.json())
-    .then (result => {
-      for(let i = 0; i < result.features.length; i++) {
-        for(let j = i+1; j < result.features.length; j++) {
-          if(result.features[i].properties.qty_search < result.features[j].properties.qty_search) {
-            let temp = result.features[i];
-            result.features[i] = result.features[j];
-            result.features[j] = temp;
+    fetch("https://utemap.com/admin")
+      .then((res) => res.json())
+      .then((result) => {
+        for (let i = 0; i < result.features.length; i++) {
+          for (let j = i + 1; j < result.features.length; j++) {
+            if (
+              result.features[i].properties.qty_search <
+              result.features[j].properties.qty_search
+            ) {
+              let temp = result.features[i];
+              result.features[i] = result.features[j];
+              result.features[j] = temp;
+            }
           }
         }
-      }
-      console.log(result.features);
-      setGeoLoc({
-        type:"FeatureCollection",
-        features: result.features
-      })
-    })
+        console.log(result.features);
+        setGeoLoc({
+          type: "FeatureCollection",
+          features: result.features,
+        });
+      });
     setInterval(() => {
-      let temp = JSON.parse(window.localStorage.getItem('data'));
-      let tempStart = JSON.parse(window.localStorage.getItem('start'));
-      let tempEnd = JSON.parse(window.localStorage.getItem('end'));
-      if(temp) {
+      let temp = JSON.parse(window.localStorage.getItem("data"));
+      let tempStart = JSON.parse(window.localStorage.getItem("start"));
+      let tempEnd = JSON.parse(window.localStorage.getItem("end"));
+      if (temp) {
         setDataApi(temp);
-        window.localStorage.removeItem('data');
+        window.localStorage.removeItem("data");
       }
-      
-        if(tempStart) {
-          let t = tempStart.map(a => Math.round(a * 10000)/10000 )
-          setStart({
-            ...start,
-            geometry:{
-              coordinates:t
-            }
-          });
-          window.localStorage.removeItem('start');
-          
-        }
-        if(tempEnd) {
-          let t = tempEnd.map(a => Math.round(a * 10000)/10000 )
-          setEnd({
-            ...end,
-            geometry: {
-              coordinates:t
-            }
-          });
-          window.localStorage.removeItem('end');
-        }
-    },1000);
+
+      if (tempStart) {
+        let t = tempStart.map((a) => Math.round(a * 10000) / 10000);
+        setStart({
+          ...start,
+          geometry: {
+            coordinates: t,
+          },
+        });
+        window.localStorage.removeItem("start");
+      }
+      if (tempEnd) {
+        let t = tempEnd.map((a) => Math.round(a * 10000) / 10000);
+        setEnd({
+          ...end,
+          geometry: {
+            coordinates: t,
+          },
+        });
+        window.localStorage.removeItem("end");
+      }
+    }, 1000);
     setLoading(true);
     setTimeout(() => {
       setLoading(false);
-    },3000);
-    
-  },[])
+    }, 3000);
+  }, []);
   return (
-    
-    <MyContext.Provider value={{
-      mode:mode,
-      setMode:setMode,
-      isLoading:isLoading,
-      setLoading:setLoading,
-      dataApi:dataApi,
-      setDataApi:setDataApi,
-      start:start,
-      setStart:setStart,
-      setEnd:setEnd,
-      end:end,
-      direction:direction,
-      setDirection:setDirection,
-      geoLoc:geoLoc,
-      setGeoLoc:setGeoLoc,
-      prevIdStart:prevIdStart,
-      prevIdEnd:prevIdEnd,
-      setPrevIdEnd:setPrevIdEnd,
-      setPrevIdStart:setPrevIdStart,
-      eventLis:eventLis,
-      setEventLis:setEventLis,
-      userLocation:userLocation,
-      setUserLocation:setUserLocation
-    }} >
-      {(geoLoc)?<div className="container">
-        {(isLoading)?<div className="container-spinner">
-          <div className="loader"></div>
-        </div>:null}
-        <Sidebar1 />
-        <Menu/>
-        <MapBox/>
-      </div>:null}
+    <MyContext.Provider
+      value={{
+        mode: mode,
+        setMode: setMode,
+        isLoading: isLoading,
+        setLoading: setLoading,
+        dataApi: dataApi,
+        setDataApi: setDataApi,
+        start: start,
+        setStart: setStart,
+        setEnd: setEnd,
+        end: end,
+        direction: direction,
+        setDirection: setDirection,
+        geoLoc: geoLoc,
+        setGeoLoc: setGeoLoc,
+        prevIdStart: prevIdStart,
+        prevIdEnd: prevIdEnd,
+        setPrevIdEnd: setPrevIdEnd,
+        setPrevIdStart: setPrevIdStart,
+        eventLis: eventLis,
+        setEventLis: setEventLis,
+        userLocation: userLocation,
+        setUserLocation: setUserLocation,
+      }}
+    >
+      {geoLoc ? (
+        <div className="container">
+          {isLoading ? (
+            <div className="container-spinner">
+              <div className="loader"></div>
+            </div>
+          ) : null}
+          <Sidebar1 />
+          <Menu />
+          <MapBox />
+        </div>
+      ) : null}
     </MyContext.Provider>
   );
 }
--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,102 +1,135 @@
-import React,{useContext,useState} from 'react';
-import MyContext  from '../../context/MyContext';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faMapMarkedAlt,faSatellite, faMoon,faSun,faDirections,faMapSigns } from '@fortawesome/free-solid-svg-icons';
+import React, { useContext, useState } from "react";
+import MyContext from "../../context/MyContext";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faMapMarkedAlt,
+  faSatellite,
+  faMoon,
+  faSun,
+  faDirections,
+  faMapSigns,
+} from "@fortawesome/free-solid-svg-icons";
 
 function Menu() {
-    const data = useContext(MyContext);
-    const [direction,setDirection] = useState(true)
-    const clickClose = () => {
-        document.querySelector(".menu").style.width = "0px";
-        setTimeout(() => {
-            document.querySelector(".container-menu").style.opacity = "0";
-            document.querySelector(".container-menu").style.visibility = "hidden";
-        },300);
+  const data = useContext(MyContext);
+  const [direction, setDirection] = useState(true);
+  const clickClose = () => {
+    document.querySelector(".menu").style.width = "0px";
+    setTimeout(() => {
+      document.querySelector(".container-menu").style.opacity = "0";
+      document.querySelector(".container-menu").style.visibility = "hidden";
+    }, 300);
+  };
+  const turnMode = () => {
+    let t = localStorage.getItem("mode");
+    if (
+      (t !== "mapbox://styles/tranhoang071120/ckf8g7uwq5h7919pfzhlxzwqe" &&
+        t !== "mapbox://styles/tranhoang071120/ckf2xr6kl05o21asu1omijgo9") ||
+      t === "mapbox://styles/tranhoang071120/ckf2xr6kl05o21asu1omijgo9"
+    ) {
+      localStorage.setItem(
+        "mode",
+        "mapbox://styles/tranhoang071120/ckf8g7uwq5h7919pfzhlxzwqe"
+      );
+      document.location.reload();
+    } else {
+      localStorage.setItem(
+        "mode",
+        "mapbox://styles/tranhoang071120/ckf2xr6kl05o21asu1omijgo9"
+      );
+      document.location.reload();
     }
-    const turnMode = () => {
-        let t = localStorage.getItem('mode');
-        if((t!== 'mapbox://styles/tranhoang071120/ckf8g7uwq5h7919pfzhlxzwqe' && t !== 'mapbox://styles/tranhoang071120/ckf2xr6kl05o21asu1omijgo9') || t ==="mapbox://styles/tranhoang071120/ckf2xr6kl05o21asu1omijgo9") {
-            localStorage.setItem('mode',"mapbox://styles/tranhoang071120/ckf8g7uwq5h7919pfzhlxzwqe");
-            document.location.reload();
-        } else {
-            localStorage.setItem('mode', "mapbox://styles/tranhoang071120/ckf2xr6kl05o21asu1omijgo9");
-            document.location.reload();
-        }
+  };
+  const turnDirection = () => {
+    if (
+      document.querySelector(".mapboxgl-ctrl-directions").style.opacity !== "0"
+    ) {
+      document.querySelector(".mapboxgl-ctrl-directions").style.opacity = "0";
+      document.querySelector(".mapboxgl-ctrl-directions").style.visibility =
+        "hidden";
+      setDirection(false);
+    } else {
+      setDirection(true);
+      document.querySelector(".mapboxgl-ctrl-directions").style.opacity = 1;
+      document.querySelector(".mapboxgl-ctrl-directions").style.visibility =
+        "visible";
     }
-    const turnDirection = () => {
-        if(document.querySelector(".mapboxgl-ctrl-directions").style.opacity !== '0') {
-            document.querySelector(".mapboxgl-ctrl-directions").style.opacity='0';
-            document.querySelector(".mapboxgl-ctrl-directions").style.visibility = "hidden";
-            setDirection(false);
-        } else {
-            setDirection(true);
-            document.querySelector(".mapboxgl-ctrl-directions").style.opacity=1;
-            document.querySelector(".mapboxgl-ctrl-directions").style.visibility = "visible";
-        }
-        
-    }
-    return (
-        <div className="container-menu" onClick={clickClose}>
-            <div className="menu">
-                <div className="menu__heading">
-                    <h1 className="heading-primary">UTE</h1>
-                    <h1 className="heading-maps">Maps</h1>
-                </div>
-                
-                <ul className="menu__list">
-                    <li className="menu__list__active">
-                        <div className="menu__list__item-icon">
-                            <FontAwesomeIcon icon={faMapMarkedAlt}/>
-                        </div>
-                        <p className="menu__list__item-text">Bản đồ</p>
-                    </li>
-                    <li className="menu__list__item">
-                        <div className="menu__list__item-icon">
-                            <FontAwesomeIcon icon={faSatellite}/>
-                        </div>
-                        <a className="menu__list__item-link" href={(data.start.properties.name !== null && data.start.properties.name !== '')?`https://map.harrisstudio.org/virtualtour/index.html?name="${data.start.properties.name}"`:"https://map.harrisstudio.org/virtualtour/index.html?name=%22C%E1%BB%95ng%20ch%C3%ADnh%22"} target="_blank">Vệ tinh</a>
-                    </li>
-                    <li className="menu__list__item" onClick={turnMode}>
-                        {(localStorage.getItem('mode') === 'mapbox://styles/tranhoang071120/ckf2xr6kl05o21asu1omijgo9' || (localStorage.getItem('mode')!== 'mapbox://styles/tranhoang071120/ckf8g7uwq5h7919pfzhlxzwqe' && localStorage.getItem('mode')!== 'mapbox://styles/tranhoang071120/ckf2xr6kl05o21asu1omijgo9'))?
-                        (
-                            <React.Fragment>
-                                <div className="menu__list__item-icon">
-                                    <FontAwesomeIcon icon={faMoon}/>
-                                </div>
-                                <p  className="menu__list__item-text">Chế độ tối</p>
-                            </React.Fragment>
-                        ):
-                        (
-                            <React.Fragment>
-                                <div className="menu__list__item-icon">
-                                    <FontAwesomeIcon icon={faSun}/>
-                                </div>
-                                <p className="menu__list__item-text">Chế độ sáng</p>
-                            </React.Fragment>
-                        )
-                        }
-                    </li>
-                    <li className="menu__list__item" onClick={turnDirection}>
-                        {
-                            (direction)?
-                            (<React.Fragment>
-                                <div className="menu__list__item-icon">
-                                    <FontAwesomeIcon icon={faMapSigns}/>
-                                </div>
-                                <p className="menu__list__item-text">Tắt chỉ đường</p>
-                            </React.Fragment>):
-                            <React.Fragment>
-                                <div className="menu__list__item-icon">
-                                    <FontAwesomeIcon icon={faDirections}/>
-                                </div>
-                                <p className="menu__list__item-text">Bật chỉ đường</p>
-                            </React.Fragment>
-                        }
-                    </li>
-                </ul>
-            </div>
+  };
+  return (
+    <div className="container-menu" onClick={clickClose}>
+      <div className="menu">
+        <div className="menu__heading">
+          <h1 className="heading-primary">UTE</h1>
+          <h1 className="heading-maps">Maps</h1>
         </div>
-    )
+
+        <ul className="menu__list">
+          <li className="menu__list__active">
+            <div className="menu__list__item-icon">
+              <FontAwesomeIcon icon={faMapMarkedAlt} />
+            </div>
+            <p className="menu__list__item-text">Bản đồ</p>
+          </li>
+          <li className="menu__list__item">
+            <div className="menu__list__item-icon">
+              <FontAwesomeIcon icon={faSatellite} />
+            </div>
+            <a
+              className="menu__list__item-link"
+              href={
+                data.start.properties.name !== null &&
+                data.start.properties.name !== ""
+                  ? `https://utemap.com/virtualtour?name="${data.start.properties.name}"`
+                  : "https://utemap.com/virtualtour?name=%22C%E1%BB%95ng%20ch%C3%ADnh%22"
+              }
+              target="_blank"
+            >
+              Vệ tinh
+            </a>
+          </li>
+          <li className="menu__list__item" onClick={turnMode}>
+            {localStorage.getItem("mode") ===
+              "mapbox://styles/tranhoang071120/ckf2xr6kl05o21asu1omijgo9" ||
+            (localStorage.getItem("mode") !==
+              "mapbox://styles/tranhoang071120/ckf8g7uwq5h7919pfzhlxzwqe" &&
+              localStorage.getItem("mode") !==
+                "mapbox://styles/tranhoang071120/ckf2xr6kl05o21asu1omijgo9") ? (
+              <React.Fragment>
+                <div className="menu__list__item-icon">
+                  <FontAwesomeIcon icon={faMoon} />
+                </div>
+                <p className="menu__list__item-text">Chế độ tối</p>
+              </React.Fragment>
+            ) : (
+              <React.Fragment>
+                <div className="menu__list__item-icon">
+                  <FontAwesomeIcon icon={faSun} />
+                </div>
+                <p className="menu__list__item-text">Chế độ sáng</p>
+              </React.Fragment>
+            )}
+          </li>
+          <li className="menu__list__item" onClick={turnDirection}>
+            {direction ? (
+              <React.Fragment>
+                <div className="menu__list__item-icon">
+                  <FontAwesomeIcon icon={faMapSigns} />
+                </div>
+                <p className="menu__list__item-text">Tắt chỉ đường</p>
+              </React.Fragment>
+            ) : (
+              <React.Fragment>
+                <div className="menu__list__item-icon">
+                  <FontAwesomeIcon icon={faDirections} />
+                </div>
+                <p className="menu__list__item-text">Bật chỉ đường</p>
+              </React.Fragment>
+            )}
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
 
-export default Menu
+export default Menu;
